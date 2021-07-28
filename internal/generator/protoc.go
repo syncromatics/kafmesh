e4db1733e6289c99aaa53ea09921bd7988706873
Joshua Benjamin <benjamin@syncromatics.com>
2021-07-28 09:36:30 -0700

    (+semver: fix) Update protoc usage to work with macOS and newer golang (#33)
    * (+semver: fix) Update protoc usage to work with macOS and newer golang
    
    Signed-off-by: Joshua Benjamin <joshua.benjamin@iherb.com>
    
    * Add grpc proto generation
    
    * Pin sarama at v1.27.0
    
    Signed-off-by: Joshua Benjamin <joshua.benjamin@iherb.com>
    
    * (+semver: fix) Updated go vet-recommended dependency
    
    Co-authored-by: Joshua Benjamin <joshua.benjamin@iherb.com>
    Co-authored-by: Daniel James <daniel@thzinc.com>

--- internal/generator/protoc.go
+++ internal/generator/protoc.go
@@ -3,6 +3,7 @@ package generator
 import (
 	"bytes"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"path"
@@ -11,6 +12,7 @@ import (
 	"strings"
 
 	"github.com/pkg/errors"
+	uuid "github.com/satori/go.uuid"
 )
 
 type protoOptions struct {
@@ -28,24 +30,9 @@ type file struct {
 
 // Protoc is a wrapper around the protec executable
 func Protoc(options protoOptions) error {
-	gopath, ok := os.LookupEnv("GOPATH")
-	if !ok {
-		return errors.Errorf("no GOPATH defined")
-	}
-
-	goBin := path.Join(gopath, "bin")
-
-	_, err := exec.LookPath(path.Join(goBin, "protoc-gen-go"))
-	if err != nil {
-		return errors.Wrap(err, "could not find protoc-gen-go")
-	}
-
-	pathenv, _ := os.LookupEnv("PATH")
-
 	args := []string{}
 	for _, i := range options.Includes {
-		args = append(args, "-I")
-		args = append(args, i)
+		args = append(args, fmt.Sprintf("--proto_path=%s", i))
 	}
 
 	packages := map[string][]string{}
@@ -69,38 +56,69 @@ func Protoc(options protoOptions) error {
 
 		r = strings.ReplaceAll(r, "\\", "/")
 
-		m = fmt.Sprintf("M%s=%s", strings.TrimLeft(r, "/"), m)
+		m = fmt.Sprintf("--go_opt=M%s=%s", strings.TrimLeft(r, "/"), m)
 
 		modules = append(modules, m)
 	}
 
 	for _, files := range packages {
-		pargs := append(args)
-		for _, f := range files {
-			pargs = append(pargs, f)
+		tmpDir, err := ioutil.TempDir("", "protocop")
+		if err != nil {
+			return errors.Wrap(err, "failed to get temp directory")
 		}
+
+		u2 := uuid.NewV4()
+		configFile := path.Join(tmpDir, u2.String())
 
 		output := options.Output
 		if runtime.GOOS == "windows" {
 			output = strings.ReplaceAll(output, "/", "\\")
 		}
 
-		pargs = append(pargs, fmt.Sprintf("--go_out=%s,plugins=grpc:%s", strings.Join(modules, ","), output))
+		pargs := append(args, fmt.Sprintf("--go_out=paths=source_relative:%s", output))
+		pargs = append(pargs, modules...)
+		pargs = append(pargs, files...)
+
+		contents := strings.Join(pargs, "\n")
+		err = ioutil.WriteFile(configFile, []byte(contents), os.ModePerm)
+		if err != nil {
+			return errors.Wrap(err, "failed to create config file")
+		}
 
 		errBuf := bytes.NewBuffer([]byte{})
-		cmd := exec.Command("protoc", pargs...)
-		cmd.Env = []string{
-			fmt.Sprintf("PATH=%s:%s:", pathenv, goBin),
-		}
+		cmd := exec.Command("protoc", "@"+configFile)
+		cmd.Env = os.Environ()
 		cmd.Stderr = errBuf
 
 		_, err = cmd.Output()
 		if err != nil {
-			return errors.Wrapf(err, "failed to run protoc: %s", string(errBuf.Bytes()))
+			return errors.Wrapf(err, "failed to run protoc: %s", errBuf.String())
 		}
 
 		if cmd.ProcessState.ExitCode() != 0 {
 			return errors.Errorf("protoc did not return 0")
+		}
+
+		// run proto generation for grpc objects
+		grpcContext := strings.ReplaceAll(contents, "go_out", "go-grpc_out")
+		grpcContext = strings.ReplaceAll(grpcContext, "go_opt", "go-grpc_opt")
+		err = ioutil.WriteFile(configFile+"-grpc", []byte(grpcContext), os.ModePerm)
+		if err != nil {
+			return errors.Wrap(err, "failed to create config file")
+		}
+
+		errBuf = bytes.NewBuffer([]byte{})
+		cmd = exec.Command("protoc", "@"+configFile+"-grpc")
+		cmd.Env = os.Environ()
+		cmd.Stderr = errBuf
+
+		_, err = cmd.Output()
+		if err != nil {
+			return errors.Wrapf(err, "failed to run protoc grpc: %s", errBuf.String())
+		}
+
+		if cmd.ProcessState.ExitCode() != 0 {
+			return errors.Errorf("protoc grpc did not return 0")
 		}
 	}
 
